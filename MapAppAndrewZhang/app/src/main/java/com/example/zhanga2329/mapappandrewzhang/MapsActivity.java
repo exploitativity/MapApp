ae60422a2ffd20cfbeeefaa554fcde186568a656
exploitativity <firerider521@gmail.com>
2017-05-26 09:42:38 -0700

    Andrew Zhang - Added Born Here marker.
    Forgot to commit yesterday...

--- MapAppAndrewZhang/app/src/main/java/com/example/zhanga2329/mapappandrewzhang/MapsActivity.java
+++ MapAppAndrewZhang/app/src/main/java/com/example/zhanga2329/mapappandrewzhang/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.zhanga2329.mapappandrewzhang;
 
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -39,8 +41,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng sydney = new LatLng(43.73, -79.76);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Born Here"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
+
 }
